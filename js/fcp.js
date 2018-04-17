ba5d428fa0f80cf0c1ef1e2b100a0df2483928be
m208 <38442079+m208@users.noreply.github.com>
2018-04-17 18:18:40 +0300

    Add files via upload

--- js/fcp.js
+++ js/fcp.js
@@ -2910,7 +2910,8 @@ function ShareImg() {
  	// goo.gl API
  function makeShort(longURL) 
  {
-	var clientKey = 'AIzaSyAMTVPm2GtPUIuO2_0qngwL9dfiGj5iumU';
+	var clientKey = 'AIzaSyBmYu7V4RIDRJ4WdjiHkuzZutv-m6F4LUc';
+	//var clientKey = 'AIzaSyAMTVPm2GtPUIuO2_0qngwL9dfiGj5iumU';
 	$.ajax({
         url: 'https://www.googleapis.com/urlshortener/v1/url?key='+clientKey+'',
 		type: 'POST',
